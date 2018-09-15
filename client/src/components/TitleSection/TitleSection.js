daa6b30c7ce7a9b0e9fe3e5a0241f5bb4ab0bae3
yung dev <andre.torquati@gmail.com>
2018-09-15 15:25:54 -0400

    created TitleSection react component

---
+++ client/src/components/TitleSection/TitleSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TitleSection.css"
+const TitleSection = () => (
+    <div className="wrapper">
+        <h1>New York Times Article Scrubber</h1>
+        <p>Search for and annotate articles of interest!</p>
+    </div>
+)
+
+export default TitleSection;
